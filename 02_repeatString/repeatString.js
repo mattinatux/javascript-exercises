aa76c3c2885feb020382f25fe09a73f1cf81b9b9
mattinatux <berlinbullet@gmail.com>
2021-10-19 17:18:37 -0400

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let stringNotAnArray = '';
 
+    if (num < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < num; i++) {
+            stringNotAnArray += string;
+        }
+    }
+
+    return stringNotAnArray;
 };
 
 // Do not edit below this line
