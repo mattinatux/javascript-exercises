0f06dce26b5d62629a718c7af384d0d7c80f2446
mattinatux <berlinbullet@gmail.com>
2021-10-19 17:45:44 -0500

    complete sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    // start of with a zero sum
+    let sum = 0;
+    
+    if (
+        // if not numbers
+        (!((typeof firstNum == 'number') && (typeof secondNum == 'number'))) ||
+        // or if not greater than 0
+        (!((firstNum >= 0) && (secondNum >= 0)))
+        ) {
+            // return an error
+            return 'ERROR';
+    }
 
+    // decide where to start the count up
+    if (firstNum < secondNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = secondNum; i <= firstNum; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
