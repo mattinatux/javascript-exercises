7e2b2a478df99144bbd447053bff10795eea1e2b
mattinatux <berlinbullet@gmail.com>
2021-11-09 15:47:20 -0600

    findTheOldest complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let date = new Date();
 
+    const orderedList = people.sort(function (firstPerson, secondPerson) {
+        firstPerson.yearOfDeath = firstPerson.yearOfDeath || 
+            ( date.getFullYear() );
+
+        secondPerson.yearOfDeath = secondPerson.yearOfDeath || 
+            ( date.getFullYear() );    
+
+        if ((firstPerson.yearOfDeath - firstPerson.yearOfBirth) > (secondPerson.yearOfDeath - secondPerson.yearOfBirth)) {
+            return -1; //
+        } else {
+            return 1;
+        }
+    });
+
+    return orderedList[0];
 };
 
 // Do not edit below this line
